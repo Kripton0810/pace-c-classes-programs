05b190b087d4c2620b823136dc6b97a9e3bc3d60
Kripton0810 <subhankar0810@gmail.com>
2022-03-01 19:20:08 +0530

    init switch commit

---
+++ switch.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+void main()
+{
+	int ch;
+	input:
+	printf("Input your choice : ");
+	scanf("%d",&ch);
+	switch(ch)
+	{
+		case 1:
+			printf("Monday\n");
+		break;
+		
+		case 2:
+			printf("Tuesday\n");
+		break;
+		
+		case 3:
+			printf("Wednesday\n");
+		break;
+		
+		case 4:
+			printf("Thrusday\n");
+		break;
+		
+		case 5:
+			printf("Friday\n");
+		break;
+		
+		case 6:
+			printf("Saturday\n");
+		break;
+		
+		case 7:
+			printf("Sunday\n");
+		break;
+		
+		default:
+			printf("Wrong...\n");
+			goto input;
+		break;
+		
+	}
+}
